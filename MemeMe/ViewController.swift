155ad8de84cd4c5a05f3150afee9b15235291ceb
Aaron Wagner <awagner@sibcycline.com>
2015-12-06 21:44:31 -0500

    Added UIImagePickController for album and camera
     - more tweaks to the view/constraints (text might need to be
       more dynamic to work the way I want)

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,24 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var cancelBtn: UIBarButtonItem!
     @IBOutlet weak var shareBtn: UIBarButtonItem!
     @IBOutlet weak var albumBtn: UIBarButtonItem!
     @IBOutlet weak var cameraBtn: UIBarButtonItem!
     
+    @IBOutlet weak var memeImage: UIImageView!
+    
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
+    
     override func viewDidLoad() {
         cameraBtn.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+        shareBtn.enabled = false
+        setupTextFields(topText)
+        setupTextFields(bottomText)
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            self.memeImage.image = image
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func setupTextFields(field: UITextField) {
+        let attributes = [
+            NSStrokeColorAttributeName : UIColor.blackColor(),
+            NSForegroundColorAttributeName : UIColor.whiteColor(),
+            NSFontAttributeName : UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : -3
+        ]
+        field.defaultTextAttributes = attributes
+        field.textAlignment = .Center
     }
 
-
+    @IBAction func pickFromCamera(sender: AnyObject) {
+        let imagePick = UIImagePickerController()
+        imagePick.delegate = self
+        self.presentViewController(imagePick, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickFromAlbums(sender: AnyObject) {
+        let imagePick = UIImagePickerController()
+        imagePick.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        imagePick.delegate = self
+        self.presentViewController(imagePick, animated: true, completion: nil)
+    }
 }
 
